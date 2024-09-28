b7e1520813b159b0fb63d2d96bc3590bc790fada
Berkay Kanca <berkaykanca@hotmail.com>
2024-09-29 00:42:34 +0300

    bf

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./dist/heroku-ang-app'));
 
 app.get('/*', (req,res) =>
-  res.sendFile('index.html', {root: './dist/heroku-ang-app/browser'}),
+  res.sendFile('index.html', {root: 'dist/heroku-ang-app/browser'}),
 );
 
 app.listen(process.env.PORT || 8080);
