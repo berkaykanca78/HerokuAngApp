f0668f875edac6785bc84a4502524ccf2c8c5be8
Berkay Kanca <berkaykanca@hotmail.com>
2024-09-29 00:53:45 +0300

    browser

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'dist/heroku-ang-app')));
 
 // Fallback route to handle all other requests
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist/heroku-ang-app/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/heroku-ang-app/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
